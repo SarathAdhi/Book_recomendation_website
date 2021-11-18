5ca9b65e2f112dc0cd6e4e765eb58af551ab73db
SarathAdhi <91727830+SarathAdhi@users.noreply.github.com>
2021-11-18 10:05:55 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,7 @@
 <?php
 
-	$db = mysqli_connect("localhost","root","","genre"); 	/* server name, username, password, database name */
+	/*$db = mysqli_connect("localhost","root","","genre"); server name, username, password, database name */
+	$db = mysqli_connect("remotemysql.com","rILqN3VfQi","FgGW8KuUEd","rILqN3VfQi");
 ?>
 
 <?php
