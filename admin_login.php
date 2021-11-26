ff4f19a9f988f4705cd3717eb0ab394fba10ebf8
SarathAdhi <s.sarathadhithya@gmail.com>
2021-11-26 10:59:28 +0530

    Update admin_login.php

--- admin_login.php
+++ admin_login.php
@@ -28,6 +28,7 @@
   </script>
   
   <style type="text/css">
+     @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;500;600&display=swap');
         .slider
         {
             margin-top: -20px;
